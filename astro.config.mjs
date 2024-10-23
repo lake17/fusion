8cd159339b3f7d6127440082b87ee765ebfbc720
lake17 <danmead01@hotmail.co.uk>
2024-10-23 16:57:16 +0100

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,49 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import markdoc from '@astrojs/markdoc';
+import { readdir } from "fs/promises";
+
+async function autogenSections() {
+	const sections = (
+		await readdir("./src/content/docs/", {
+			withFileTypes: true,
+		})
+	)
+		.filter((x) => x.isDirectory())
+		.map((x) => x.name);
+	return sections.map((x) => {
+		return {
+			label: x,
+			autogenerate: {
+				directory: x,
+				collapsed: true,
+			},
+		};
+	});
+}
 
 // https://astro.build/config
 export default defineConfig({
+	prefetch: {
+		defaultStrategy: 'viewport',
+		prefetchAll: true,
+	},
 	integrations: [
+		markdoc({ allowHTML: true }),
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			title: 'FusionCare Documentation',
+			customCss: ['./src/styles/custom.css',],
+			logo: {
+				light: './src/assets/light-logo.svg',
+				dark: './src/assets/dark-logo.svg',
+				replacesTitle: true,
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			sidebar: await autogenSections(),
+			components: {
+				Sidebar: './src/components/Sidebar.astro'
+			},
+			pagination: false,
 		}),
 	],
 });
---
+++ markdoc.config.mjs
@@ -0,0 +1,12 @@
+import { defineMarkdocConfig, component } from "@astrojs/markdoc/config"
+import starlightMarkdoc from "@astrojs/starlight-markdoc"
+
+// https://docs.astro.build/en/guides/integrations-guide/markdoc/
+export default defineMarkdocConfig({
+  extends: [starlightMarkdoc()],
+  tags: {
+    FrontPage: {
+      render: component("./src/components/FrontPage.astro"),
+    },
+  },
+})
